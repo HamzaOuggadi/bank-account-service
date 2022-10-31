3c6f578fd324c3a1ecdf5d0e19bd5f2aecf360c7
Hamza Ouggadi <45943796+OddToast@users.noreply.github.com>
2022-10-31 10:31:53 +0100

    Third commit : -GraphQl Controller Added. -Exception For No Account Found Added.

--- src/main/java/ma/enset/bankaccountservice/web/BankAccountGraphQLController.java
+++ src/main/java/ma/enset/bankaccountservice/web/BankAccountGraphQLController.java
@@ -3,6 +3,8 @@ package ma.enset.bankaccountservice.web;
 import ma.enset.bankaccountservice.entities.BankAccount;
 import ma.enset.bankaccountservice.repositories.BankAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 
 import java.util.List;
@@ -12,7 +14,12 @@ public class BankAccountGraphQLController {
     @Autowired
     private BankAccountRepository bankAccountRepository;
 
+    @QueryMapping
     public List<BankAccount> accountList() {
         return bankAccountRepository.findAll();
+    }
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id) {
+        return bankAccountRepository.findById(id).orElseThrow(()-> new RuntimeException(String.format("Account %s Not Found", id)));
     }
 }
